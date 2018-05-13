af4fb33bb076f1ae25536af4a79daabfa7fbcc79
btaks <btaks@avans.nl>
2018-05-13 22:55:49 +0200

    Fixed app.get

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ let port = 8080;
 
 app.get('/', function(reg, res) {
     res.send('Home');
-});
+})
 
 app.all('*', function(req, res) {
     res.status(404);
     res.send('404 - Not found');
-});
+})
 
 app.listen(port, function() {
     console.log('Server listening @ ' + port);
-});
+})
