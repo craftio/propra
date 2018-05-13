ef4a7125ffdcdc3cea890431bffbec5d59b900ea
btaks <btaks@avans.nl>
2018-05-13 22:59:17 +0200

    Fix 2

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const app = express;
+const app = express();
 
 let port = 8080;
 
-app.get('/', function(reg, res) {
+app.get('/', function (reg, res) {
     res.send('Home');
 })
 
-app.all('*', function(req, res) {
+app.all('*', function (req, res) {
     res.status(404);
     res.send('404 - Not found');
 })
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('Server listening @ ' + port);
 })
