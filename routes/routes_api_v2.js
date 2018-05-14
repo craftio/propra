07d5e5089ff78e5762a332e9ce2aa257ecf8ff9e
btaks <btaks@avans.nl>
2018-05-14 02:44:14 +0200

    Added URL query parameter for category.

--- routes/routes_api_v2.js
+++ routes/routes_api_v2.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const recipes = require('../recipes.js');
 
 router.get('/info', (req, res) => {
     res.status(200);
@@ -9,10 +10,29 @@ router.get('/info', (req, res) => {
     });
 });
 
-router.get('/recipes', (req, res) => {
+router.get('/recipes/:number', (req, res, next) => {
+    res.json(recipes[req.params.number-1]);
+});
+
+router.get('/recipes/:category?', (req, res, next) => {
+    const category = req.query.category || '';
+
+    let result = [];
+    if (category === '') {
+        result = recipes;
+    } else {
+        result = recipes.filter(function (item) {
+            return (item.category === category);
+        });
+    }
+
+    res.json(result);
+});
+
+router.get('/recipes', (req, res,) => {
     res.status(200);
-    res.json(require('../recipes.js'));
-})
+    res.json(recipes);
+});
 
 router.get('*', (req, res) => {
     res.status(404);
